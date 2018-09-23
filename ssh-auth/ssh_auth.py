525070e04765e7036a9a6595a0b7eb7baca1af5f
System Administrator <root@juxicn-mbp.local>
2018-09-23 23:24:08 +0800

    noisy

--- ssh-auth/ssh_auth.py
+++ ssh-auth/ssh_auth.py
@@ -21,55 +21,24 @@ class SSHAuth(object):
         print data
         return data
 
-    def gen_hosts(self, data):
-        pass
+    def gen_hosts(self, nodes):
+        fd = open('./playbook/hosts', 'w+')
+        fd.write('[cluster]\n')
+        for node in nodes:
+            host_line = '{} ansible_ssh_user=bismog ansible_ssh_pass=***'.format(node)
+            fd.write(host_line)
+        fd.close()
 
     def callback(self, data):
         a = Auth()
         a.run()
 
 def main():
-    # Deploy Etcd cluster
-    # c = Compose()
-    # compose_temp = '{}/docker-compose.d/docker-compose.etcd1.yml.template'.format(PATH)
-    # print(compose_temp)
-    # c.gen_compose_file(compose_temp)
-    # # compose_file = '{}docker-compose.d/docker-compose.etcd1.yml'.format(PATH)
-    # compose_file = '{}/docker-compose.d/docker-compose.single-node.yml'.format(PATH)
-    # c.run(compose_file)
-    # print('run Etcd ok')
-    d = Deploy()
-    d.run()
 
-    # # Append nodes
-    # d = Data()
-    # nodes = ["192.168.3.45"]
-    # snodes = json.dumps(nodes)
-    # d.put(snodes, key='nodes')
-    # # print(d.get(key='nodes'))
-    # print('Update data ok')
-    # data = json.loads(d.get(key='nodes'))
-    # # print(type(data))
-
-    # # Setup mutual authorization
-    # # pb = Playbook(data, playbooks=['./playbook/auth.yml'])
-    # # print(pb.inventory)
-    # # # hack for verbosity
-    # # from ansible.utils.display import Display
-    # # display = Display(verbosity=7)
-    # # import __main__ as main
-    # # setattr(main, "display", display)
-
-    # # # import pdb;pdb.set_trace()
-    # # pb.run()
-    # # print('Auth ok')
-
-    # a = Auth()
-    # a.run()
-
-    # 
     ssh_auth = SSHAuth()
-    ssh_auth.watch('192.168.3.45', 2379, 'nodes')
+    while True:
+        ssh_auth.watch('localhost', 2379, 'nodes')
+        ssh_auth.callback()
 
 if __name__ == "__main__":
     main()
