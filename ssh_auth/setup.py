b3b1c7357e7b25231897665eec2d06f7c21515b2
root <root@host.localdomain>
2018-10-07 09:53:17 -0400

    10.7

--- ssh_auth/setup.py
+++ ssh_auth/setup.py
@@ -1,10 +1,25 @@
 from setuptools import setup
-setup(name='ssh-auth',
+from setuptools.command.install import install
+
+class myinstall(install):
+    """ My Installation scripts"""
+    def run(self):
+        print "Hello, this is my installation script"
+        install.run(self)
+
+setup(name='ssh_auth',
     version='0.1',
-    descrition='Setup SSH mutual authorization with etcd key-value',
+    description='Setup SSH mutual authorization with etcd key-value',
     url='https://github.com/bismog/ssh-auth',
     author='bismog',
     author_email='bismogg@gmail.com',
     license='MIT',
-    packages=['ssh-auth', 'python-etcd'],
+    packages=['ssh_auth'],
+    package_data={
+        'ssh_auth':['playbook.d/*',],
+    },
+    scripts=['ssh_auth/etc/systemd/system/multi-user.target.wants/ssh_auth.service'],
+    cmdclass={
+        'install': myinstall,
+    },
     zip_safe=False)
